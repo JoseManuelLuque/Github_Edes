02d75d746d1d4b978d8c1165adef98b0709b50e2
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-02-15 22:41:20 +0100

    Resolución Ticket AA0002

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -1,3 +1,20 @@
 class Tipo(tipo: String) {
 
+    val tiposValidos = setOf("acero", "agua", "bicho", "dragon", "electrico", "fantasma", "fuego", "hada", "hielo", "lucha", "normal", "planta", "psiquico", "roca", "siniestro", "tierra", "veneno", "volador")
+
+    var tipo: String = ""
+
+        get() = field
+
+        set(value) {
+            if (tiposValidos.contains(value)) {
+                field = value
+            } else {
+                throw IllegalArgumentException("El tipo $value no es válido")
+            }
+        }
+    init {
+        this.tipo = tipo
+    }
+
 }

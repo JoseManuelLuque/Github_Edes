f4613065a0946badc63824299966f9f548f6ee08
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-02-16 16:34:28 +0100

    Pausa en rama AA0004

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -17,4 +17,20 @@ class Tipo(tipo: String) {
         this.tipo = tipo
     }
 
+
+
+    fun Efectividad(tipoAtaque: Tipo, tipoRecibe: Tipo): Double {
+
+        when(tipoAtaque.tipo == "acero"){
+            (tipoRecibe.tipo == "hielo" || tipoRecibe.tipo == "roca" || tipoRecibe.tipo == "hada") -> return 2.0
+            (tipoRecibe.tipo == "acero" || tipoRecibe.tipo == "agua" || tipoRecibe.tipo == "electrico" || tipoRecibe.tipo == "fuego") -> return 0.5
+            (tipoRecibe.tipo == "bicho" || tipoRecibe.tipo == "fantasma" || tipoRecibe.tipo == "lucha" || tipoRecibe.tipo == "normal" || tipoRecibe.tipo =="planta" || tipoRecibe.tipo == "psiquico" || tipoRecibe.tipo == "siniestro" || tipoRecibe.tipo == "tierra" || tipoRecibe.tipo == "veneno" || tipoRecibe.tipo == "volador") -> return 1.0
+            else -> print("")
+        }
+
+        when(tipoAtaque.tipo == "agua"){
+            (tipoRecibe.tipo == "fuego" || tipoRecibe.tipo == "tierra" || tipoRecibe.tipo == "roca") -> return 2.0
+        }
+    }
+
 }
